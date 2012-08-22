e05b0a636411039e498f149fa797e3e985fc6489
Tim Whitlock <r-github@timwhitlock.info>
2012-08-22 11:44:40 +0100

    improved compression with sandboxing function in compiled output

--- bin/compile.php
+++ bin/compile.php
@@ -12,6 +12,7 @@ require $base.'/php/JSCompiler.php';
 Cli::init();
 Cli::register_arg('c', 'compile', 'Script paths to compile; delimit with ":"', true );
 Cli::register_arg('s', 'search',  'Search paths for js modules; delimit with ":"', false );
+Cli::register_arg('n', '',        'Disables compression for debugging', false );
 Cli::register_arg('h', 'help',    'Show this help text', false );
 Cli::validate_args(); 
 
@@ -30,10 +31,17 @@ try {
     // compile source code and dependencies
     $data = $Compiler->compile() or Cli::death('Nothing compiled');
     
-    // concatentate all source code starting with helper
-    $src = file_get_contents( $base.'/js/common.js' );
+    // concatentate all source code starting with helper and sandbox
+    $src = "( function( window, document, undefined ){\n".file_get_contents( $base.'/js/common.js' );
     foreach( $data as $d ){
         $src .= "\n".$d['js'];
+    }
+    $src .= "\n} )( window, document );\n";
+    
+    // output uncompressed source if -n flag was specified
+    if( Cli::arg('n') ){
+        echo $src;
+        exit(0);
     }
 
     // Have uncompressed source code - run it through Closure compiler to compress
