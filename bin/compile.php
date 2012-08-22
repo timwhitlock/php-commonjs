b92e01d9141cbe97419ecb1ceceffa24db7171c2
Tim Whitlock <r-github@timwhitlock.info>
2012-08-22 12:48:01 +0100

    added cache disablment to cli

--- bin/compile.php
+++ bin/compile.php
@@ -13,6 +13,7 @@ Cli::init();
 Cli::register_arg('c', 'compile', 'Script paths to compile; delimit with ":"', true );
 Cli::register_arg('s', 'search',  'Search paths for js modules; delimit with ":"', false );
 Cli::register_arg('n', '',        'Disables compression for debugging', false );
+Cli::register_arg('d', '',        'Disables cache for debugging', false );
 Cli::register_arg('h', 'help',    'Show this help text', false );
 Cli::validate_args(); 
 
@@ -22,6 +23,11 @@ try {
     // instantiate copmpiler with optional search paths
     $search_paths = Cli::arg('s');
     $Compiler = new JSCompiler( $search_paths );
+    
+    // disable cache if -d flag set
+    if( Cli::arg('d') ){
+        $Compiler->disable_cache();
+    }
     
     // add scripts passed
     foreach( explode(':', Cli::arg('c') ) as $path ){
